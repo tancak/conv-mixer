105453c20c65cb10dadb9298bdf92fa6f50a7029
Marek <marek.tancak16@gmail.com>
2021-12-04 15:08:36 +0000

    Update SLURM script

--- Code/utils.py
+++ Code/utils.py
@@ -1,11 +1,11 @@
 import torch
-from koila import LazyTensor, lazy
+#from koila import LazyTensor, lazy
 
 def validate(model, test_loader, loss_function, device):
     test_loss = []
     with torch.no_grad():
         for data in test_loader:
-            inputs, labels = lazy(data[0]).to(device), lazy(data[1]).to(device)
+            inputs, labels = data[0].to(device), data[1].to(device)
             outputs = model(inputs)
             loss = loss_function(outputs,labels)
             test_loss.append(loss.item())
@@ -18,7 +18,7 @@ def accuracy(model, dataloader, device):
 
     with torch.no_grad():
         for data in dataloader:
-            inputs, labels = lazy(data[0]).to(device), lazy(data[1]).to(device)
+            inputs, labels = data[0].to(device), data[1].to(device)
             outputs = model(inputs)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
